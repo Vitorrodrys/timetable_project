2d262c53cfbc9776fed54273cd3429679b3fb091
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-05-18 16:59:50 -0300

    fix: register models class

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Course)
-admin.site.register(models.CourseSection)
+admin.site.register(models.Discipline)
 admin.site.register(models.Environment)
 admin.site.register(models.Professor)
-admin.site.register(models.Subject)
+admin.site.register(models.SchoolClass)
+admin.site.register(models.TimeSlot)
+admin.site.register(models.TimeSlotSchema)
